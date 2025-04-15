888b57d5bccdce6ee7ce18c659c3f3d599e22303
Valentin Georgiev <valentin.py77@gmail.com>
2025-04-15 15:55:50 +0300

    jeopardy.py

--- jeopardy.py
+++ jeopardy.py
@@ -1,12 +1,12 @@
-#The dataset I am working with contains data from the game show Jeopardy!. Each row in the dataset represents a question (or “clue”) from the show, along with various attributes related to that question. Here’s a breakdown of what the dataset includes:
-#Dataset Components:
-#Show Number: The episode number of the show.
-#Air Date: The date the episode aired.
-#Round: The round of the game (e.g., Jeopardy, Double Jeopardy, Final Jeopardy).
-#Category: The category under which the question falls.
-#Value: The monetary value of the question, indicating its difficulty level.
-#Question: The actual text of the question or clue.
-#Answer: The correct response to the question.
+""" The dataset I am working with contains data from the game show Jeopardy!. Each row in the dataset represents a question (or “clue”) from the show, along with various attributes related to that question. Here’s a breakdown of what the dataset includes:
+Dataset Components:
+Show Number: The episode number of the show.
+Air Date: The date the episode aired.
+Round: The round of the game (e.g., Jeopardy, Double Jeopardy, Final Jeopardy).
+Category: The category under which the question falls.
+Value: The monetary value of the question, indicating its difficulty level.
+Question: The actual text of the question or clue.
+Answer: The correct response to the question. """
 
 import pandas as pd
 import re
